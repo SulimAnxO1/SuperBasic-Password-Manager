0d5d975fb9bad82c2d09f982c8007df608cd946d
SulimAnxO1 <mxsulaimansufian@gmail.com>
2025-10-12 03:59:59 +0600

    js code done almost

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,19 +1,70 @@
-str = `<tr>
-<td>${website}</td>
-<td>${website}</td>
-<td>${website}</td>
-</tr>`;
+function copyText(txt) {
+  navigator.clipboard.writeText(txt).catch(() => {
+    console.warn("Clipboard copying failed");
+  });
+}
 
+const deletePassword = (website) => {
+  let data = localStorage.getItem("passwords");
+  let arr = JSON.parse(data);
+  arrUpdated = arr.filter((e) => {
+    return e.website != website;
+  });
+  localStorage.setItem("passwords", JSON.stringify(arrUpdated));
+  showPasswords();
+};
+
+const showPasswords = () => {
+  let table = document.querySelector("table");
+  let data = localStorage.getItem("passwords");
+  if (data == null || JSON.parse(data).length == 0) {
+    table.innerHTML = "No Data To Show";
+  } else {
+    table.innerHTML = `
+        <tr>
+          <th>Website Url</th>
+          <th>Username</th>
+          <th>Password</th>
+          <th>Actions</th>
+        </tr>`;
+    let arr = JSON.parse(data);
+    let str = "";
+    for (let index = 0; index < arr.length; index++) {
+      const element = arr[index];
+      str += `<tr>
+<td>${element.website} <img onclick="copyText('${element.website}')" src="/Image/copy.svg" alt="Copy Button"></td>
+<td>${element.username} <img onclick="copyText('${element.username}')" src="/Image/copy.svg" alt="Copy Button"></td>
+<td>${element.password} <img onclick="copyText('${element.password}')" src="/Image/copy.svg" alt="Copy Button"></td>
+<td><button class="btnsm" onclick="deletePassword('${element.website}')">Delete</button></td>
+</tr>`;
+    }
+    table.innerHTML = table.innerHTML + str;
+  }
+  website.value = "";
+  username.value = "";
+  password.value = "";
+};
+
+showPasswords();
 document.querySelector(".btn").addEventListener("click", (e) => {
   e.preventDefault();
   let passwords = localStorage.getItem("passwords");
   if (passwords == null) {
     let json = [];
-    json.push({ username: username.value, password: password.value });
+    json.push({
+      website: website.value,
+      username: username.value,
+      password: password.value,
+    });
     localStorage.setItem("passwords", JSON.stringify(json));
   } else {
     let json = JSON.parse(localStorage.getItem("passwords"));
-    json.push({ username: username.value, password: password.value });
+    json.push({
+      website: website.value,
+      username: username.value,
+      password: password.value,
+    });
     localStorage.setItem("passwords", JSON.stringify(json));
   }
+  showPasswords();
 });
