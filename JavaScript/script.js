0675e0e427525efc146e2dd6a23d593e9e9f266a
SulimAnxO1 <mxsulaimansufian@gmail.com>
2025-10-12 04:10:23 +0600

    project done

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,11 @@
+function maskPassword(pass) {
+  let str = "";
+  for (let index = 0; index < pass.length; index++) {
+    str += "*";
+  }
+  return str;
+}
+
 function copyText(txt) {
   navigator.clipboard.writeText(txt).catch(() => {
     console.warn("Clipboard copying failed");
